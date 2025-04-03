a27e95cb5cf576b882da549eaf0572a513e65f74
gi0-h <hwicksky2@naver.com>
2025-04-03 18:58:33 +0900

    타임리프로 뷰, 삭제, 수정, 생성 추가

--- src/main/java/me/springbootdeveloper/domain/Article.java
+++ src/main/java/me/springbootdeveloper/domain/Article.java
@@ -5,7 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter //클래스 필드에 대해 별도 코드 없이 모든 필드에 대한 접근자 메서드 생성 가능
 @NoArgsConstructor(access = AccessLevel.PROTECTED) //접근 제어자가 protected인 기본 생성자 생성
@@ -13,7 +19,7 @@ public class Article {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = true)
+    @Column(name = "id", updatable = false)
     private Long id;
 
     @Column(name = "title", nullable = false)
@@ -32,6 +38,14 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    @CreatedDate //엔티티가 생성될 때 생성 시간 저장
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate //엔티티가 수정될 때 수정 시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     //@Getter, @NoArgsConstructor로 대체
 //    protected Article() { //기본 생성자
